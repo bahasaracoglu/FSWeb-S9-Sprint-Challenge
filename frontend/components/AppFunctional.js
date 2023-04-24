7ac9b4d71358c2c72b22d07f9e43f053844fb68d
bahasaracoglu <bahasaracoglu@gmail.com>
2023-04-24 21:39:38 +0300

    kutu hareket ediyor. ( sınır dışına çıkabiliyor)

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,16 +1,56 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 // önerilen başlangıç stateleri
-const initialMessage = ''
-const initialEmail = ''
-const initialSteps = 0
-const initialIndex = 4 //  "B" nin bulunduğu indexi
+const initalState = {
+  initialMessage: "",
+  initialEmail: "",
+  initialSteps: 0,
+  initialIndex: 4, //  "B" nin bulunduğu indexi
+};
 
 export default function AppFunctional(props) {
+  const [state, setState] = useState(initalState);
+
   // AŞAĞIDAKİ HELPERLAR SADECE ÖNERİDİR.
   // Bunları silip kendi mantığınızla sıfırdan geliştirebilirsiniz.
 
-  function getXY() {
+  let cord;
+  switch (state.initialIndex) {
+    case 0:
+      cord = "(1, 1)";
+      break;
+    case 1:
+      cord = "(2, 1)";
+      break;
+    case 2:
+      cord = "(3, 1)";
+      break;
+    case 3:
+      cord = "(1, 2)";
+      break;
+    case 4:
+      cord = "(2, 2)";
+      break;
+    case 5:
+      cord = "(3, 2)";
+      break;
+    case 6:
+      cord = "(1, 3)";
+      break;
+    case 7:
+      cord = "(2, 3)";
+      break;
+    case 8:
+      cord = "(3, 3)";
+      break;
+    default:
+    // code block
+  }
+
+  console.log(cord);
+
+  function getXY(index) {
     // Koordinatları izlemek için bir state e sahip olmak gerekli değildir.
     // Bunları hesaplayabilmek için "B" nin hangi indexte olduğunu bilmek yeterlidir.
   }
@@ -24,11 +64,35 @@ export default function AppFunctional(props) {
   function reset() {
     // Tüm stateleri başlangıç ​​değerlerine sıfırlamak için bu helperı kullanın.
   }
-
+  useEffect(
+    function sonrakiIndex(yon) {
+      // Bu helper bir yön ("sol", "yukarı", vb.) alır ve "B" nin bir sonraki indeksinin ne olduğunu hesaplar.
+      // Gridin kenarına ulaşıldığında başka gidecek yer olmadığı için,
+      // şu anki indeksi değiştirmemeli.
+      if (yon === "left") {
+        setState({ ...state, initialIndex: state.initialIndex - 1 });
+      }
+      console.log(state);
+      console.log(yon);
+    },
+    [state.initialIndex]
+  );
   function sonrakiIndex(yon) {
     // Bu helper bir yön ("sol", "yukarı", vb.) alır ve "B" nin bir sonraki indeksinin ne olduğunu hesaplar.
     // Gridin kenarına ulaşıldığında başka gidecek yer olmadığı için,
     // şu anki indeksi değiştirmemeli.
+    if (yon === "left") {
+      setState({ ...state, initialIndex: state.initialIndex - 1 });
+    } else if (yon === "right") {
+      setState({ ...state, initialIndex: state.initialIndex + 1 });
+    } else if (yon === "up") {
+      setState({ ...state, initialIndex: state.initialIndex - 3 });
+    } else if (yon === "down") {
+      setState({ ...state, initialIndex: state.initialIndex + 3 });
+    }
+
+    console.log(state);
+    console.log(yon);
   }
 
   function ilerle(evt) {
@@ -51,28 +115,39 @@ export default function AppFunctional(props) {
         <h3 id="steps">0 kere ilerlediniz</h3>
       </div>
       <div id="grid">
-        {
-          [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
-            </div>
-          ))
-        }
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
+          <div
+            key={idx}
+            className={`square${idx === state.initialIndex ? " active" : ""}`}
+          >
+            {idx === state.initialIndex ? "B" : null}
+          </div>
+        ))}
       </div>
       <div className="info">
         <h3 id="message"></h3>
       </div>
       <div id="keypad">
-        <button id="left">SOL</button>
-        <button id="up">YUKARI</button>
-        <button id="right">SAĞ</button>
-        <button id="down">AŞAĞI</button>
-        <button id="reset">reset</button>
+        <button id="left" onClick={() => sonrakiIndex("left")}>
+          SOL
+        </button>
+        <button id="up" onClick={() => sonrakiIndex("up")}>
+          YUKARI
+        </button>
+        <button id="right" onClick={() => sonrakiIndex("right")}>
+          SAĞ
+        </button>
+        <button id="down" onClick={() => sonrakiIndex("down")}>
+          AŞAĞI
+        </button>
+        <button id="reset" onClick={() => sonrakiIndex("reset")}>
+          reset
+        </button>
       </div>
       <form>
         <input id="email" type="email" placeholder="email girin"></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
-  )
+  );
 }
